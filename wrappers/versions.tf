92da577548329a8039f1d350426fe29f84d4fce2
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-03-20 14:47:55 +0100

    feat: add wrapper module for multi-instance Terragrunt support (#53)
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>

---
+++ wrappers/versions.tf
@@ -0,0 +1,14 @@
+# Terraform version
+terraform {
+  required_version = ">= 1.10.0"
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = ">= 2.70.0"
+    }
+  }
+
+  provider_meta "digitalocean" {
+    module_name = "terraform-do-modules/terraform-digitalocean-database"
+  }
+}
